7462ab46cfd5fd205d139754b7f60b71747d4192
Faosy Adebimpe <faosyadebimpe@gmail.com>
2025-06-22 14:00:01 -0700

    increase express body size limit

--- src/index.js
+++ src/index.js
@@ -26,7 +26,8 @@ app.use(
         allowedHeaders: ['Content-Type', 'Authorization'],
     })
 );
-app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 app.use(cookieParser());
 // app.use(express.urlencoded({ extended: true }));
 
