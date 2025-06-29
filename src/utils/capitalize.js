fd10082cab2605491afc62fa63bb4d2bd83cbaae
Faosy Adebimpe <faosyadebimpe@gmail.com>
2025-06-29 06:17:07 -0700

    add international local comment to capitalize.js

--- src/utils/capitalize.js
+++ src/utils/capitalize.js
@@ -1,5 +1,6 @@
 const capitalize = (value) => {
     const parsedValue = value.trim();
+    // international locale
     const firstCharacter = parsedValue[0].toLocaleUpperCase();
     const otherCharacters = parsedValue.slice(1).toLocaleLowerCase();
     const newValue = `${firstCharacter}${otherCharacters}`;
